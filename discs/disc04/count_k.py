379d441869bcfb5fbf34728f094f7895ba9c108b
whoslcy <whoslcy@foxmail.com>
2022-10-01 16:20:24 +0800

    disc04 completed except for Q5

---
+++ discs/disc04/count_k.py
@@ -0,0 +1,23 @@
+def count_k(n, k):
+    """ Counts the number of paths up a flight of n stairs
+    when taking up to and including k steps at a time.
+    >>> count_k(3, 3) # 3, 2 + 1, 1 + 2, 1 + 1 + 1
+    4
+    >>> count_k(4, 4)
+    8
+    >>> count_k(10, 3)
+    274
+    >>> count_k(300, 1) # Only one step at a time
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    else:
+        path_number = 0
+        for step in range(1, k+1):
+            path_number += count_k(n-step, k)
+        return path_number
+    
--- test.py
+++
@@ -1,8 +0,0 @@
-countdown = 10
-many_players_times = []
-while countdown:
-    one_player_times = []
-    many_players_times += one_player_times
-    countdown -= 1
-
-print(many_players_times)
