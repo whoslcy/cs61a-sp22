bcde8621676ea697999d5e92df7b6c2ef155119e
whoslcy <whoslcy@foxmail.com>
2022-11-10 16:03:45 +0800

    Lab07: questions before q2(included) completed

---
+++ labs/lab07/parsons_probs/reverse_link.py
@@ -0,0 +1,51 @@
+def reverse_link(lnk):
+    """
+    Given a linked list lnk, return a new linked list which has all the
+    elements of lnk but in reverse order.
+    
+    >>> s = Link(1, Link(2, Link(3, Link.empty)))
+    >>> reverse_link(s)
+    Link(3, Link(2, Link(1)))
+    >>> s
+    Link(1, Link(2, Link(3)))
+    >>> k = Link(3, Link(5, Link(7, Link(9))))
+    >>> reverse_link(k)
+    Link(9, Link(7, Link(5, Link(3))))
+    >>> k
+    Link(3, Link(5, Link(7, Link(9))))
+    """
+    "*** YOUR CODE HERE ***"
+    reversed_lnk_head = Link(None)
+    original_lnk = lnk
+    while original_lnk is not Link.empty:
+        old_reversed_lnk = reversed_lnk_head.rest
+        new_reversed_lnk = Link(original_lnk.first)
+        new_reversed_lnk.rest = old_reversed_lnk
+        reversed_lnk_head.rest = new_reversed_lnk
+        original_lnk = original_lnk.rest
+    return reversed_lnk_head.rest
+        
+
+
+
+class Link:
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
