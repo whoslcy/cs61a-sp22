6813528f52821e0ba7c9123814f064b1f7aa7299
whoslcy <whoslcy@foxmail.com>
2022-11-16 14:30:28 +0800

    lab10 completed

--- labs/lab10/lab10.scm
+++ labs/lab10/lab10.scm
@@ -1,9 +1,31 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    'YOUR-CODE-HERE
+    (cond 
+        ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        ((> num1 num2) 1)
+    )
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 'YOUR-CODE-HERE
+    (define (adder inc)
+        (+ inc num)
+    )
+    adder
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 'YOUR-CODE-HERE
+    (lambda (x) (f (g x)))
+)
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 'YOUR-CODE-HERE
+    (if (= exp 1)
+        base
+        (if (even? exp)
+            (square (pow base (/ exp 2)))
+            (* base (pow base (- exp 1)))
+        )
+    )
+)
