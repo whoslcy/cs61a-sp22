3c988f05cc3bd4eeff9c28730a8941d2a472cdbe
whoslcy <whoslcy@foxmail.com>
2022-11-17 15:04:44 +0800

    hw06 completed

--- hws/hw06/hw06.scm
+++ hws/hw06/hw06.scm
@@ -31,6 +31,23 @@
         ((and (null? lst1) (null? lst2))
             nil)))
 
-(define (my-filter func lst) 'YOUR-CODE-HERE)
+(define (my-filter func lst) 'YOUR-CODE-HERE
+    (if (null? lst)
+        lst
+        (if (func (car lst))
+            (cons (car lst) (my-filter func (cdr lst)))
+            (my-filter func (cdr lst)))))
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst) 'YOUR-CODE-HERE
+    (if (null? lst)
+        lst
+        (let
+            (
+                (current_node (car lst))
+            )
+            (cons 
+                current_node 
+                (no-repeats (my-filter (lambda (node) (not (= node current_node))) (cdr lst))))
+            )
+    )
+)
