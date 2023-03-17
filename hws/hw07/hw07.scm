4a1f7aec6a69c2aca421b77a450a12f31eb77e1b
whoslcy <whoslcy@foxmail.com>
2023-03-17 12:49:34 +0800

    [hw07]: Completed

--- hws/hw07/hw07.scm
+++ hws/hw07/hw07.scm
@@ -1,32 +1,58 @@
 (define (cadr lst) (car (cdr lst)))
 
 (define (make-kwlist1 keys values)
-  'YOUR-CODE-HERE)
+  (list keys values))
 
-(define (get-keys-kwlist1 kwlist) 'YOUR-CODE-HERE)
+(define (get-keys-kwlist1 kwlist)
+  (car kwlist))
 
 (define (get-values-kwlist1 kwlist)
-  'YOUR-CODE-HERE)
+  (cadr kwlist))
 
 (define (make-kwlist2 keys values)
-  'YOUR-CODE-HERE)
+  (if (and (not (null? keys)) (not (null? values)))
+    (cons (list (car keys) (car values)) (make-kwlist2 (cdr keys) (cdr values)))
+    nil))
 
-(define (get-keys-kwlist2 kwlist) 'YOUR-CODE-HERE)
+(define (get-keys-kwlist2 kwlist)
+  (if (null? kwlist)
+    nil
+    (cons (car (car kwlist)) (get-keys-kwlist2 (cdr kwlist)))))
 
 (define (get-values-kwlist2 kwlist)
-  'YOUR-CODE-HERE)
+  (if (null? kwlist)
+    nil
+    (cons (cadr (car kwlist)) (get-values-kwlist2 (cdr kwlist)))))
 
 (define (add-to-kwlist kwlist key value)
-  'YOUR-CODE-HERE)
+  (make-kwlist 
+    (append (get-keys-kwlist kwlist) (list key)) 
+    (append (get-values-kwlist kwlist) (list value))))
 
 (define (get-first-from-kwlist kwlist key)
-  'YOUR-CODE-HERE)
+  (let (
+    (keys (get-keys-kwlist kwlist))
+    (values (get-values-kwlist kwlist)))
+    (cond 
+      ((and (null? keys) (null? values)) nil)
+      ((equal? key (car keys)) (car values))
+      (else (get-first-from-kwlist (make-kwlist (cdr keys) (cdr values)) key)))))
 
 (define (prune-expr expr)
-  (define (prune-helper lst) 'YOUR-CODE-HERE)
-  'YOUR-CODE-HERE)
+  (define (prune-helper lst)
+    (if (null? lst)
+      nil
+      (if (null? (cdr lst))
+        lst
+        (append (list (car lst)) (prune-helper (cdr (cdr lst)))))))
+  (append (list (car expr)) (prune-helper (cdr expr))))
 
-(define (curry-cook formals body) 'YOUR-CODE-HERE)
+(define (curry-cook formals body)
+  (if (null? (cdr formals))
+    (list `lambda formals body)
+    (list `lambda (list (car formals)) (curry-cook (cdr formals) body))))
 
 (define (curry-consume curries args)
-  'YOUR-CODE-HERE)
+  (if (null? args)
+    curries
+    (curry-consume (curries (car args)) (cdr args))))
